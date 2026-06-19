dc33d9d741483ca3afef1d65863a0e22a606dee7
unknown <rizlee@mybizniche.com>
2026-06-20 01:08:33 +0800

    services page

--- template-parts/footer-template.php
+++ template-parts/footer-template.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * Default Footer Template content.
+ * Footer Template Block Template.
  *
- * This file syncs to the "Footer Template" Block Template post on theme activation.
- * Edit the Block Template post in WordPress admin, then export back to this file
- * using the export button in the admin.
+ * Syncs with "Footer Template" Block Template post.
+ * Edit in WordPress admin, then export using Block Templates → Sync Tools.
  *
  * @package CustomTheme
  */
@@ -13,4 +12,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
-<!-- wp:mbn-theme/site-footer {"menuId1":5,"menuId2":6} /-->
+<!-- wp:mbn-theme/site-footer {"gravityFormShortcode":"[gravityform id=\u00221\u0022 title=\u0022false\u0022]","socialLinks":[{"label":"Facebook","url":"#","icon":"facebook"},{"label":"Instagram","url":"#","icon":"instagram"},{"label":"X","url":"#","icon":"x"},{"label":"LinkedIn","url":"#","icon":"linkedin"},{"label":"YouTube","url":"#","icon":"youtube"}]} /-->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
