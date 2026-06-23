b689dffe5673d090b69e93c16bc16807c2cfdd75
unknown <rizlee@mybizniche.com>
2026-06-23 12:02:15 +0800

    menu update

--- template-parts/nav-menus/main-menu.php
+++ template-parts/nav-menus/main-menu.php
@@ -12,65 +12,62 @@
  * @package CustomTheme
  */
 
-return array (
-  'name' => 'Main Menu',
-  'slug' => 'main-menu',
-  'locations' => 
-  array (
-    0 => 'primary-menu',
-  ),
-  'items' => 
-  array (
-    0 => 
-    array (
-      'title' => 'About',
-      'type' => 'post_type',
-      'object' => 'page',
-      'url' => 'https://mysite.dev.local/about/',
-      'target' => '',
-      'attr_title' => '',
-      'description' => '',
-      'classes' => 
-      array (
-      ),
-      'xfn' => '',
-      'order' => 1,
-      'parent_index' => -1,
-      'object_slug' => 'about',
-    ),
-    1 => 
-    array (
-      'title' => 'Services',
-      'type' => 'post_type',
-      'object' => 'page',
-      'url' => 'https://mysite.dev.local/services/',
-      'target' => '',
-      'attr_title' => '',
-      'description' => '',
-      'classes' => 
-      array (
-      ),
-      'xfn' => '',
-      'order' => 2,
-      'parent_index' => -1,
-      'object_slug' => 'services',
-    ),
-    2 => 
-    array (
-      'title' => 'Motorcycle Suspension Services',
-      'type' => 'post_type',
-      'object' => 'page',
-      'url' => 'https://mysite.dev.local/services/motorcycle-suspension-services/',
-      'target' => '',
-      'attr_title' => '',
-      'description' => '',
-      'classes' => 
-      array (
-      ),
-      'xfn' => '',
-      'order' => 3,
-      'parent_index' => 1,
-      'object_slug' => 'motorcycle-suspension-services',
-    ),
-  ),
+return array(
+	'name'      => 'Main Menu',
+	'slug'      => 'main-menu',
+	'locations' =>
+	array(
+		0 => 'primary-menu',
+	),
+	'items'     =>
+	array(
+		0 =>
+		array(
+			'title'        => 'About',
+			'type'         => 'post_type',
+			'object'       => 'page',
+			'url'          => 'https://mysite.dev.local/about/',
+			'target'       => '',
+			'attr_title'   => '',
+			'description'  => '',
+			'classes'      =>
+			array(),
+			'xfn'          => '',
+			'order'        => 1,
+			'parent_index' => -1,
+			'object_slug'  => 'about',
+		),
+		1 =>
+		array(
+			'title'        => 'Services',
+			'type'         => 'post_type',
+			'object'       => 'page',
+			'url'          => 'https://mysite.dev.local/services/',
+			'target'       => '',
+			'attr_title'   => '',
+			'description'  => '',
+			'classes'      =>
+			array(),
+			'xfn'          => '',
+			'order'        => 2,
+			'parent_index' => -1,
+			'object_slug'  => 'services',
+		),
+		2 =>
+		array(
+			'title'        => 'Motorcycle Suspension Services',
+			'type'         => 'post_type',
+			'object'       => 'page',
+			'url'          => 'https://mysite.dev.local/services/motorcycle-suspension-services/',
+			'target'       => '',
+			'attr_title'   => '',
+			'description'  => '',
+			'classes'      =>
+			array(),
+			'xfn'          => '',
+			'order'        => 3,
+			'parent_index' => 1,
+			'object_slug'  => 'motorcycle-suspension-services',
+		),
+	),
 );
