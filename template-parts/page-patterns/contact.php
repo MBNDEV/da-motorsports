31ffe57348e51e028eb12b9417d21255b7e84dce
Amil Tan <amil@vyde.io>
2026-06-25 06:25:24 +0800

    fix array issue in page patterns

--- template-parts/page-patterns/contact.php
+++ template-parts/page-patterns/contact.php
@@ -22,7 +22,7 @@ return array(
 	'template'           => "page-templates\/template-blank.php",
 	'featured_image_url' => "",
 	'featured_image_path' => "", // Theme assets path (ships via Git)
-	'custom_fields'      => {"_wp_page_template":"page-templates\/template-blank.php"},
+	'custom_fields'      => array( '_wp_page_template' => 'page-templates/template-blank.php' ),
 	'content'            => <<<'EOD'
 <!-- wp:mbn-theme/contact {"heroBgPhotoId":199,"heroBgPhotoUrl":"http://localhost/wordpress/wp-content/uploads/2026/06/hero-bg-photo.jpg","heroBgTextureId":197,"heroBgTextureUrl":"http://localhost/wordpress/wp-content/uploads/2026/06/hero-bg-texture-1.png","heroDividerId":202,"heroDividerUrl":"http://localhost/wordpress/wp-content/uploads/2026/06/hero-divider-line-1.svg","contactMascotId":206,"contactMascotUrl":"http://localhost/wordpress/wp-content/uploads/2026/06/contact-mascot-1.png","contactDividerId":200,"contactDividerUrl":"http://localhost/wordpress/wp-content/uploads/2026/06/contact-divider-line.svg","gravityFormShortcode":"[gravityform id="2" title="false"]","testimonialBgId":196,"testimonialBgUrl":"http://localhost/wordpress/wp-content/uploads/2026/06/cta-bg-texture.png","ctaBgPhotoId":198,"ctaBgPhotoUrl":"http://localhost/wordpress/wp-content/uploads/2026/06/cta-bg-photo.jpg","ctaBgTextureId":196,"ctaBgTextureUrl":"http://localhost/wordpress/wp-content/uploads/2026/06/cta-bg-texture.png","ctaDividerId":204,"ctaDividerUrl":"http://localhost/wordpress/wp-content/uploads/2026/06/cta-divider-line.svg"} /-->
 EOD
