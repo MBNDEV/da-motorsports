4f0e0dc09e0c0acae4f3de53603eb746d2d29a20
Amil Tan <amil@vyde.io>
2026-06-25 04:49:11 +0800

    fix lint

--- blocks/service-page/render.php
+++ blocks/service-page/render.php
@@ -309,7 +309,7 @@ $wrapper_attributes = get_block_wrapper_attributes( array( 'class' => 'homepage'
                 if ( empty( $client_name ) ) {
                   $client_name = get_the_title();
                 }
-                $avatar_url      = get_the_post_thumbnail_url( get_the_ID(), 'thumbnail' );
+                $avatar_url = get_the_post_thumbnail_url( get_the_ID(), 'thumbnail' );
                 if ( ! $avatar_url ) {
                   $avatar_url = $theme_uri . '/build/blocks/service-page/assets/images/avatar-marcus-reid.jpg';
                 }
